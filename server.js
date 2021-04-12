9356f766f363ad0c426f9a6b6182a529a0d7fd59
Tarun Singh <82393306+Flash2022@users.noreply.github.com>
2021-04-12 23:48:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if(process.env.NODE_ENV === 'production'){
   //static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req,res) => {
+  app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname,'client','build', 'index.html'))
   });
 
